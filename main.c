a1c0bf81ee783d70dae133605cc5f1a0fc5cd573
dixe <niko.fris@hotmail.com>
2014-02-08 23:30:32 +0100

    fixed main int to pointer warrning

--- main.c
+++ main.c
@@ -5,19 +5,34 @@
 
 #include "instlist.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-int  main(){
+int main(int argc, char **argv){
 
   struct instlist* list = list_init();
-  struct node* node = NULL;
 
-  node = list_malloc_node(COMMA);
-  add_node(list, node);
-  node = list_malloc_node(COMMA);
-  add_node(list, node);
-  list_print(list);
+  FILE *file;
 
+  if(argc != 2){
+    printf("Usage: %s file.exe\n", argv[0]);
+    exit(1);
+  }
+  file = fopen(argv[1],"r");
 
+  if(file == NULL){
+    printf("File == NULL");
+    exit(1);
+  }
+  else{
+    // everything is good, we can parse the program, then print the
+    // instruction list
+    parse_prog(file,list);
+    list_print(list);
+  }
+
+  fclose(file);
+
+  free_list(list);
 
   return 0;
 }
