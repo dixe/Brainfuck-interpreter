70990a251bfa9125be74984baa1c64e87f145998
dixe <niko.fris@hotmail.com>
2014-02-09 03:04:44 +0100

    added support for nested loops

--- interpret.c
+++ interpret.c
@@ -5,18 +5,18 @@
 
 // return 0 on succes interpretation, and -1 on error
 #include "instlist.h"
+#include "interpret.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node* loop(char* bpointer,  struct node* tmp_node);
+struct node* loop2(char* bpointer,  struct loop_* loop);
 
-char* find_loop_end(char* bpointer_, struct node* tmp_node);
+struct loop_* create_loop(struct node* node);
 
 int interpret(struct instlist* list){
 
   // allocate memory for the brainfuck environment
   char* bpointer = malloc(2048); // malloc 2 mbyte
-  const char* start = bpointer; // used to make use we stay in our memory space
   struct node* node = list->head; // init node to head
 
   //init to alle to 0
@@ -59,8 +59,8 @@ int interpret(struct instlist* list){
         bpointer--;
         break;
       case LSTART:
-        find_loop_end(bpointer, node);
-        node =loop(bpointer,node);
+        //node = loop(bpointer,node);
+        node = loop2(bpointer, create_loop(node));
         break;
       case LEND:
         //DO nothing gets handles in loop() function
@@ -85,89 +85,84 @@ int interpret(struct instlist* list){
 //function to handle loop
 // takes the global brainfcuk pointer and a new loop pointer
 // loop pointer points to end of loop
-struct node* loop(char* bpointer,  struct node* tmp_node ){
-  struct node* node = tmp_node; // save the node used in interpret
+struct node* loop2(char* bpointer,  struct loop_* loop){
+  struct node* node = loop->start;
 
   while(node != NULL){
     switch(node->data){
-      case COMMA:
-        *bpointer = getchar();
-        /* when useing getchar(), the enter is still in buffer
-         * if we did not do this, the next , would give
-         * the user a change to input something
-         */
-        fflush(stdin);
-        break;
-      case VINC:
-        *bpointer += 1;
-        break;
-      case DOT:
-        putchar(*bpointer);
-        break;
-      case VDEC:
-        *bpointer -= 1;
-        break;
-      case PINC:
-        bpointer++;
-        break;
-      case PDEC:
-        bpointer--;
-        break;
-      case LSTART:
-        find_loop_end(bpointer, node);
-        //        loop(bpointer);
-        break;
-      case LEND:
+    case COMMA:
+      *bpointer = getchar();
+      /* when useing getchar(), the enter is still in buffer
+       * if we did not do this, the next , would give
+       * the user a change to input something
+       */
+      fflush(stdin);
+      break;
+    case VINC:
+      *bpointer += 1;
+      break;
+    case DOT:
+      putchar(*bpointer);
+      break;
+    case VDEC:
+      *bpointer -= 1;
+      break;
+    case PINC:
+      bpointer++;
+      break;
+    case PDEC:
+      bpointer--;
+      break;
+    case LSTART:
+      if(node != loop->start){
+        node = loop2(bpointer, create_loop(node));
+      }
+      break;
+    case LEND:
+      //if ( node == loop->end){ // if it is not the end, keep going
         if(*bpointer !=0){
-          node = tmp_node; // rest back to start of loop code
+          node = loop->start; // rest back to start of loop code
         }
         else{
-          return node; // else return and continiue in interpret
+          return node; //loop->end; // else return and continiue in interpret
         }
-        break;
-
-      default:
-        printf("default print, we need some cases");
-        break;
-      }
-
+        //      }
+      break;
+    default:
+      printf("default print, we need some cases");
+      break;
+    }
     //next instruction
     node = node->next;
 
   }
 
-
+  // should not return here, since that means there where no LEND
+  exit(1);
 }
 
+// create a loop, this find the right loop end
+struct loop_* create_loop(struct node* node){
+  struct loop_* loop = (struct loop_*) malloc(sizeof(struct loop_*));
+  loop->start = node;
+  // start at -1, since the current node is a LSTART
+  // and nest gets incremented
+  int nest=-1;
 
-// return a pointer to the end of the loop
-char* find_loop_end(char* bpointer, struct node* tmp_node) {
-  struct node* node = tmp_node; // we don't want do move the node pointer used in interpret
-  char* lp = bpointer; //set return pointer to current bpointer
-  int done = 0;
-  int nest = 0; // keep track of nested loops
-
-
-  while(node->next != NULL && !done){
-    if( node->data == LEND ){
-      if(nest == 0){
-        done = 1; // we found an loop end
-      }
-      else{
-        // we found a new loop end
-        nest--;
-      }
+  while(node !=NULL){
+    if (node->data == LEND && nest == 0){
+      loop->end = node;
+      return loop;
     }
-    else{
-      if(node->data == LSTART){
-        nest++;
-      }
-      node = node->next;
-      //increment the return pointer to next address
-      lp++;
+    else if(node->data == LEND){
+      nest--;
     }
+    else if(node->data == LSTART){
+      nest++;
+    }
+    node = node->next;
   }
 
-  return lp;
-
+  printf("could not find nested loop ending ]");
+  exit(1);
 }
--- interpret.h
+++ interpret.h
@@ -6,7 +6,17 @@
 #ifndef INTERPRET_H
 #define  INTERPRET_H
 
+#include "parser.h" // need struct node
+#include "instlist.h"
+
+struct loop_{
+  struct node* end; // the node that end this loop
+  struct node* start; // start of loop
+};
+
 // return 0 on succes interpretation, and -1 on error
 int interpret(struct instlist* list);
+
+
 
 #endif /* INTERPRET_H */
