92cc1350e4147ecb52a3d081ce8e8fcde0464aa0
Nikolaj Østergaard <niko.fris@gmail.com>
2014-02-09 23:20:39 +0100

    updated comments

--- parser.h
+++ parser.h
@@ -9,7 +9,7 @@
 #include "instlist.h"
 #include <stdio.h>
 
-// takes a file pointer and a instruction list, pointer returns nothing
+// takes a file pointer and a instruction list pointer
 void parse_prog(FILE *fp, struct instlist* list);
 
 #endif /*PARSER_H*/
